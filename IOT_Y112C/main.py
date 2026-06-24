cca86cfb44ce32e2ee546ad4b217584137a5ecdc
khanhnhseigniteprovidersite-stack <khanhnhse.igniteprovidersite@gmail.com>
2026-06-24 11:30:58 +0700

    Update collection box IP

--- IOT_Y112C/main.py
+++ IOT_Y112C/main.py
@@ -9,26 +9,26 @@ if __name__ == '__main__':
         file_path = get_file_path(config['folder_path']).replace('.xlsx', '')
         save_latest_path(file_path)
         time.sleep(1)
-    
+
     close_excel_file(file_path)
     time.sleep(5)
     print('Init Excel Manager...!')
     excel = CSVDataManager(file_path)
-    plc._write_single(1000, 1, register_type='DM') #noted
+    plc._write_single(config['ready_register'], 1, register_type='DM')
     print('Program Ready!')
     while True:
-        change_signal = plc._read_single(500, register_type='DM') #noted --> DM
+        change_signal = plc._read_single(config['change_file_register'], register_type='DM')
         if change_signal == 1:
-            plc._write_single(500, 0, register_type='DM')
+            plc._write_single(config['change_file_register'], 0, register_type='DM')
             file_path = get_file_path(config['folder_path'])
             save_latest_path(file_path)
             excel = CSVDataManager(file_path)
             time.sleep(2)
-        
+
         data_update_dict = plc.process()
         if data_update_dict:
             close_excel_file(file_path)
             for key_name, data in data_update_dict.items():
                 excel.update_data[key_name](data)
-        plc._write_single(1000, 1, register_type='DM') #noted
+        plc._write_single(config['ready_register'], 1, register_type='DM')
         time.sleep(0.5)
